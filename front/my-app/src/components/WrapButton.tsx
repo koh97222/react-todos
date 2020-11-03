a33df7e59bea7c180ea770be045dd773ea16ae48
koh97222 <koh97222@gmail.com>
2020-11-03 09:47:45 +0900

    material-uiを導入

---
+++ front/my-app/src/components/WrapButton.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Button } from "@material-ui/core";
+
+const WrapButton = () => {
+  return (
+    <>
+      <Button variant="contained" color="secondary">
+        Hello World!
+      </Button>
+    </>
+  );
+};
+
+export default WrapButton;
