9472270492db37c3662fc4982307dda0df2c490e
koh97222 <koh97222@gmail.com>
2020-12-06 14:08:00 +0900

    React.FCについて

--- front/my-app/src/components/WrapButton.tsx
+++ front/my-app/src/components/WrapButton.tsx
@@ -12,8 +12,23 @@ interface Buttonprops {
 /**
  * Buttonをラップしたコンポーネント
  * @param props
+ * @see https://github.com/typescript-cheatsheets/react#section-2-getting-started
  */
-const WrapButton = (props: Buttonprops) => {
+// "通常の関数" バージョンとのいくつかの違い。
+// 通常の関数バージョンは暗黙的ですが、React.FunctionComponentは戻り値の型を明示的に指定します（もしくは追加のアノテーションが必要です）。
+// displayName、propTypes、defaultProps などの静的プロパティの型チェックとオートコンプリートを提供します。
+// defaultPropsをReact.FunctionComponentで使用する際にいくつかの既知の問題があることに注意してください。
+// 子の暗黙の定義を提供します(下記参照) - しかし、暗黙の子の型にはいくつかの問題があり(例: DefinitelyTyped#33006)、子を消費するコンポーネントについては明示的にした方が良いかもしれません。
+// usage ）
+// const Heading: React.FC = (props) => {
+//   <h1>{children}</h1>
+// }
+
+// const ParentComponent: React.FC = () => {
+//   <Heading>Hello World!</Heding>
+// }
+// children を使用するとタグ内に記述された JSX またはテキストにアクセスすることが可能.
+const WrapButton: React.FC<Buttonprops> = (props) => {
   const useStyles = makeStyles({
     field: {
       height: props.height,
